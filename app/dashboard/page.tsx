050e0440ce75d1a333c3ca52795825f50150af83
briyan torrez <briyan.torrez@agetic.gob.bo>
2024-09-24 17:12:20 -0400

    feat: crear array de objetos todo

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,47 +1,90 @@
 "use client"
 
 import Iconify from "@/components/iconify"
-import { Accordion, AccordionItem, Divider } from "@nextui-org/react"
+import { Accordion, AccordionItem, Button, Divider, Textarea } from "@nextui-org/react"
 
 export default function IndexPage() {
+
+  const listExample = [
+    {
+      key: 1,
+      title: 'Accordion 1',
+      subtitle: 'Subtitle 1',
+      complete: false,
+    },
+    {
+      key: 2,
+      title: 'Accordion 2',
+      subtitle: 'Summary 2',
+      complete: true,
+    },
+    {
+      key: 3,
+      title: 'Item 3',
+      subtitle: 'Summary 3',
+      complete: true,
+    },
+    {
+      key: 4,
+      title: 'Item 4',
+      subtitle: 'Description 4',
+      complete: true,
+    },
+    {
+      key: 5,
+      title: 'Module 5',
+      subtitle: 'Subtitle 5',
+      complete: false,
+    },
+    {
+      key: 6,
+      title: 'Panel 6',
+      subtitle: 'Subtitle 6',
+      complete: true,
+    }
+  ]
+
   return (
     <div className="flex justify-center">
       <div className="w-[720px]">
         <Accordion variant="splitted">
-          <AccordionItem
-            key="1"
-            aria-label="Accordion 1"
-            subtitle="Press to expand"
-            title="Titulo"
-            startContent={
-              <Iconify icon='akar-icons:radio' />
-            }
-          >
-            Contenido por defecto
-          </AccordionItem>
-          <AccordionItem
-            key="2"
-            aria-label="Accordion 2"
-            subtitle={
-              <span>
-                Press to expand <strong>key 2</strong>
-              </span>
-            }
-            title={<span className="line-through">Accordion 2</span>}
-            startContent={
-              <Iconify icon='akar-icons:radio-fill' color="success" />
-            }
-            classNames={
-              {
-                content: "pb-5 px-2",
+          {listExample.map(item => (
+            <AccordionItem
+              key={item.key}
+              aria-label={`Accordion ${item.key}`}
+              subtitle={
+                <span>
+                  Presiona para expandir
+                </span>
               }
-            }
-          >
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente officiis sit exercitationem cupiditate minus.
-          </AccordionItem>
-          <AccordionItem key="3" aria-label="Accordion 3" subtitle="Press to expand" title="Accordion 3">
-            lookign for algo
-          </AccordionItem>
+              title={<span className="line-through">{item.title}</span>}
+              startContent={
+                <Iconify icon={item.complete ? 'akar-icons:radio-fill' : 'akar-icons:radio'} color={item.complete ? 'success' : 'default'} />
+              }
+              classNames={
+                {
+                  content: "pb-5 px-2",
+                }
+              }
+            >
+              <div>
+              <Textarea
+                isReadOnly
+                fullWidth
+                label="Notas"
+                variant="bordered"
+                labelPlacement="outside"
+                placeholder="Enter your description"
+                defaultValue="NextUI is a React UI library that provides a set of accessible, reusable, and beautiful components."
+              />
+            </div>
+            <div className="flex justify-end mt-5">
+              <Button color="danger">
+                Eliminar
+              </Button>
+            </div>
+            </AccordionItem>
+          ))}
         </Accordion>
       </div>
     </div>
